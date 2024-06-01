348b4f370aa1b9dfdcb2e5521e407bc5c2ce7ca2
rizkifadilah12 <rf8740072@gmail.com>
2024-06-01 09:39:00 +0700

    first-commit

--- src/component/index.ts
+++ src/component/index.ts
@@ -3,5 +3,4 @@ import KategoriAndTopUp from "./KategoriAndTopUp";
 import SliderMobile from "./mobile/SliderMobile";
 import SimpleSlider from "./Slider";
 import MobileDesain from "./mobile";
-import Product from "./product";
-export { Header, KategoriAndTopUp, SimpleSlider, SliderMobile, MobileDesain, Product };
+export { Header, KategoriAndTopUp, SimpleSlider, SliderMobile, MobileDesain };
